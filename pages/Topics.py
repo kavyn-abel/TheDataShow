9a481c614738228cdb26b52f1bc5a85774786c6b
kavyn-abel <kavynabel@gmail.com>
2025-08-20 11:14:20 -0600

    Update Topics.py

--- pages/Topics.py
+++ pages/Topics.py
@@ -62,7 +62,7 @@ st.dataframe(data = birthday_df.head(), hide_index=True)
         divider_line()
 
         st.write('With this data we can find out things such as which year had the most births.')
-        st.write('We can do this by summing the number of births within each year. We can look at the results in the chart below (If you hover over a bar in the chart, it will show you the number of births for that year.):')
+        st.write('We can do this by summing the number of births within each year. We can look at the results in the chart below (If you hover over a bar in the chart, it will show you the number of births for that year):')
 
         # Get the number of births per year
         years_df = birthday_df.groupby("year").sum().reset_index()
@@ -376,7 +376,7 @@ highest_vote_avg_and_count1 = highest_vote_avg_and_count[highest_vote_avg_and_co
         bom_df2 = bom_df1.sort_values('Box Office Margin', ascending = False)
         # st.table(bom_df2.head(10))
 
-        st.write('Here is a chart showing the top 5 movies with that criteria.')
+        st.write('Here is a chart showing the top 5 movies with that criteria. (You can hover over a bar to get the exact Box Office Margin)')
 
         # Chart for most profitable movies
         bom_movie_data = bom_df2.head(5)
@@ -863,6 +863,7 @@ elif topic == '⚓ The Titanic':
     if st.button('Finished? Click Here.'):
         st.success("Nice job exploring data! 🙌 Want to try another path? Just pick another topic at the top.")
         st.snow()
+
 
 
 
